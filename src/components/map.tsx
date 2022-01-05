26fc1e71ef98a012671c104bd80b2d92ac82ac32
Craig McLean <craig.mclean39@gmail.com>
2022-01-05 14:54:09 -0800

    Filter map features and display only those required
    Added a temp filter to the initial mapData, then a projection is created
    that fits only the filtered features.

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -2,6 +2,11 @@ import * as d3 from 'd3';
 import { useRef, useEffect, useState } from 'react';
 import geoJson from '../data/mapdata.json';
 
+interface fColl {
+  type: string;
+  features: any[];
+}
+
 const Map = () => {
   const svgRef = useRef<SVGSVGElement>(null);
   const [pathElements, setPathElements] = useState(null);
@@ -13,6 +18,9 @@ const Map = () => {
     .fitSize([width, height], geoJson as any);
   let pathGenerator: any = d3.geoPath().projection(projection);
 
+  // The geoJSON file takes a while to load, even though it is a local file, it's quite large
+  // this useEffect will kickoff the convertGeoJsonToPaths function after we are sure
+  // that the json file is loaded
   useEffect(() => {
     async function fetchMapData() {
       await fetch('../data/mapdata.json');
@@ -20,10 +28,36 @@ const Map = () => {
     }
 
     fetchMapData();
-  });
+  }, []);
 
   const covertGeoJsonToPaths = () => {
-    const paths = geoJson.features.map((d, idx) => {
+    // When calling the fitSize function on the projection, we need a collection of features,
+    // this is the format that the function expects
+    let featureCollection = {
+      type: 'FeatureCollection',
+      features: [],
+    } as fColl;
+
+    // Here we can filter features based on any info, then add them to the feature collection
+    // This will allow us to fit only the filtered features in the map
+    let filteredFeatures = geoJson.features.filter((d, idx) => {
+      if (d.properties.continent === 'North America') {
+        featureCollection.features.push(d);
+        return true;
+      }
+      return false;
+    });
+
+    // console.log(filteredFeatures);
+
+    // create new project and pathGenerator based on the features in the featureCollection
+    projection = d3
+      .geoMercator()
+      .fitSize([width, height], featureCollection as any);
+    pathGenerator = d3.geoPath().projection(projection);
+
+    // create the individual paths
+    const paths = filteredFeatures.map((d, idx) => {
       return (
         <path
           key={'path' + idx}
